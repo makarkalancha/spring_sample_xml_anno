f489b6112b94d64058733e7648281615e16d30b8
Makar Kalancha <makarkalancha@gmail.com>
2017-09-22 14:28:48 -0400

    xml

---
+++ spring_sample_xml_anno/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -0,0 +1,33 @@
+package com.pluralsight.service;
+
+import java.util.List;
+
+import com.pluralsight.model.Customer;
+import com.pluralsight.repository.CustomerRepository;
+
+public class CustomerServiceImpl implements CustomerService {
+	private CustomerRepository customerRepository;
+
+	public CustomerServiceImpl() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public CustomerServiceImpl(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.pluralsight.service.CustomerService#findAll()
+	 */
+	@Override
+	public List<Customer> findAll() {
+		return customerRepository.findAll();
+
+	}
+
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+}
