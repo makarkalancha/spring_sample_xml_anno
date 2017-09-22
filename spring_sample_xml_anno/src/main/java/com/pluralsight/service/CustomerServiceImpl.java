229a73e9c96636413992f93d256f5a2ae7d74a4a
Makar Kalancha <makarkalancha@gmail.com>
2017-09-22 15:28:23 -0400

    annotations

--- spring_sample_xml_anno/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ spring_sample_xml_anno/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,20 +2,18 @@ package com.pluralsight.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Service;
+
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
+	@Autowired
 	private CustomerRepository customerRepository;
 
-	public CustomerServiceImpl() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public CustomerServiceImpl(CustomerRepository customerRepository) {
-		this.customerRepository = customerRepository;
-	}
-	
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -25,9 +23,5 @@ public class CustomerServiceImpl implements CustomerService {
 	public List<Customer> findAll() {
 		return customerRepository.findAll();
 
-	}
-
-	public void setCustomerRepository(CustomerRepository customerRepository) {
-		this.customerRepository = customerRepository;
 	}
 }
